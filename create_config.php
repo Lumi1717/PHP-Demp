dc544d2c4ef8e18c21af3bc4885d4c740557ae7f
Lumi1717 <ayhahlam623@gmail.com>
2023-12-21 22:46:13 +0400

    deleted code

--- create_config.php
+++ create_config.php
@@ -43,35 +43,4 @@
         header('Location: view.php');
     }
 
-
-    // if(isset($_POST['submit'])){
-    
-    //     $first_name = $_POST['firstname'];
-    //     $last_name = $_POST['lastname'];
-    //     $email = $_POST['email'];
-    //     $password = $_POST['password'];
-    //     $gender = $_POST['gender'];
-    
-    //     echo"YUH!";
-        
-        
-    //     echo "First Name: $first_name<br>";
-    //     echo "Last Name: $last_name<br>";
-    //     echo "Email: $email<br>";
-    //     echo "Password: $password<br>";
-    //     echo "Gender: $gender<br>";
-
-    //     $sql = "INSERT INTO `users` (`firstname`, `lastname`, `email`, `gender`, `password`)
-    //     VALUES ('$first_name', '$last_name', '$email', '$gender', '$password')";
-        
-
-    //     echo "SQL Query: $sql<br>"; 
-    //     $result = $conn->query($sql);
-        
-    //     if($result == TRUE){
-    //         echo "New record created successfully !";
-    //     }else{
-    //         echo "Error: " .$sql. "<br>". $conn->error;
-    //     }
-    //     }
 ?>
